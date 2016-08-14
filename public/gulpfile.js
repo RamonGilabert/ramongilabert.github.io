c6ce45fc99ca5db53e8da98139f56c474c51822b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-14 13:51:05 +0200

    Adds the perfect setup

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+var stylus = require('gulp-stylus');
 var mustache = require('gulp-mustache-plus');
 
-// Gulp Mustache Task
-gulp.task('mustache', function() {
-    gulp.src("./mustache/*.mustache")
-        .pipe(mustache({},{},{
-            file_1: "partials/*.mustache"
-        })).pipe(gulp.dest("./"));
+gulp.task('stylus', function () {
+  gulp.src('./stylus/*.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['mustache'], function () {
-    gulp.watch('./mustache/{,*/}*.{mustache}', ['mustache']);
+gulp.task('mustache', function() {
+  gulp.src("./mustache/*.mustache")
+    .pipe(mustache({},{}, {
+      head: "./mustache/partials/head.mustache",
+      footer: "./mustache/partials/footer.mustache",
+    })).pipe(gulp.dest("./"));
 });
+
+gulp.task('watch', ['stylus', 'mustache'], function() {
+  gulp.watch('./stylus/**/*.styl', ['stylus']);
+  gulp.watch('./mustache/**/*.mustache', ['mustache']);
+});
+
+gulp.task('default', ['stylus', 'mustache', 'watch']);
