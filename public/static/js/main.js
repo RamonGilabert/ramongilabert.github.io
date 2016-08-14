1f053799db7835c116e530cd785602eab47ce9be
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-14 18:05:40 +0200

    Adds the data from JSON

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,1 +1,36 @@
 // JavaScript gilabert.design
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  function loadJSON(response) {
+
+    var object = new XMLHttpRequest
+    object.overrideMimeType("application/json");
+    object.open('GET', './data/manifesto/manifesto.json', true);
+    object.onreadystatechange = function () {
+      if (object.readyState == 4) {
+        response(object.responseText);
+      }
+    };
+
+    object.send(null);
+  }
+
+  function mustache() {
+    loadJSON(function(response) {
+      var template = document.getElementById('first-template').innerHTML;
+      var section = document.getElementById('manifesto-explanation');
+      var data = [];
+
+      try {
+        data = JSON.parse(response);
+      } catch(error) {
+        console.log(error)
+      }
+
+      section.innerHTML = Mustache.render(template, data);
+    });
+  }
+
+  mustache()
+});
