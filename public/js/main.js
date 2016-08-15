faa02d8c5a7178eb521ba38549822210e55ce53a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-15 13:32:11 +0200

    Secures email for not receiving SPAM

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
   var email = document.getElementById('email-link');
 
   email.addEventListener('click', function() {
-    email.href = 'mailto:ramon@gilabert.design';
+    var letter = decode("znvygb:enzba@tvynoreg.qrfvta");
+    var subject = decode("Uryyb gurer! :)");
+    var reference = letter + '?subject=' + subject;
+
+    email.href = reference;
   });
+
+  function decode(letters) {
+    return letters.replace(/[a-zA-Z]/g, function(c) {
+      return String.fromCharCode((c <= "Z" ? 90 : 122)
+      >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    })
+  }
 });
