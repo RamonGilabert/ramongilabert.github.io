6ee36614f7ec443af8954fbb23639676bb57a17a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-15 15:20:40 +0200

    Generates the dots with JavaScript

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var email = document.getElementById('email-link');
+  var separator = new Separator('dot-separator');
+
+  separator.generate();
 
   email.addEventListener('click', function() {
     var letter = decode("znvygb:enzba@tvynoreg.qrfvta");
@@ -18,4 +21,43 @@ document.addEventListener('DOMContentLoaded', function() {
       >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
     })
   }
+
+  window.addEventListener('resize', function() {
+    separator.generate();
+  });
 });
+
+function Separator(name) {
+
+  this.elementName = name;
+  this.wrapper = document.getElementById(this.elementName);
+  this.lastWidth = window.innerWidth;
+
+  this.generate = function() {
+    if ((window.innerWidth >= 1400
+      || Math.abs(this.lastWidth - window.innerWidth) <= 10)
+      && this.wrapper.innerHTML != '') {
+      return
+    }
+
+    var windowWidth = window.innerWidth >= 1400 ? 1400 : window.innerWidth;
+    var width = windowWidth - 57;
+    var times = Math.floor(width / 11) * 2;
+    var values = [true, true, true, true, true, true, false];
+
+    this.wrapper.innerHTML = '';
+
+    for (var i = 0; i <= times; i++) {
+      var dot = document.createElement('div');
+      dot.classList.add('dot');
+
+      this.wrapper.appendChild(dot);
+
+      if (!values[Math.floor(Math.random() * values.length)] || i <= 5) {
+        dot.style.opacity = 0
+      }
+    }
+
+    this.lastWidth = window.innerWidth;
+  }
+}
