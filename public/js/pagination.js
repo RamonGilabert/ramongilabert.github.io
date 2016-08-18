bf0d3bc73a99e9b960858bd2a79b02e7ac8a3900
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-18 18:30:52 +0200

    Adds the main scrolling for the site

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -2,47 +2,154 @@
 
 window.addEventListener('load', function() {
 
-  var keys = {
-    32 : 1,
-    37 : 1,
-    38 : 1,
-    39 : 1,
-    40 : 1
-  };
+  prepareWindowFrames();
 
-  function preventDefault(event) {
-    event = event || window.event;
+  var animating = false;
+  var safety = 500;
+  var sections = document.getElementsByTagName('section');
+  var myself = document.getElementById('sections');
+  var position = Math.abs(myself.scrollTop / window.innerHeight);
+  swipe(myself);
 
-    if (event.preventDefault) {
+  document.addEventListener('swipeUp', function(event) {
+    scrollUp()
+  });
+
+  document.addEventListener('swipeDown', function(event) {
+    scrollDown()
+  });
+
+  window.addEventListener('DOMMouseScroll', scrollHandler);
+  window.addEventListener('mousewheel', scrollHandler);
+
+  function scrollHandler(event) {
+  	event.preventDefault();
+  	var delta = event.wheelDelta || -event.detail;
+
+    if (animating) {
       event.preventDefault();
+      return;
     }
 
-    event.returnValue = false;
+    animating = true;
+		delta > 0 ? scrollUp() : scrollDown();
   }
 
-  function preventDefaultForScrollKeys(event) {
-    if (keys[event.keyCode]) {
-      preventDefault(event);
-      return false;
+  function scrollUp() {
+    if (position == 0) {
+      animating = false;
+      return;
     }
+
+    position = position - 1;
+    scroll(position * window.innerHeight);
   }
 
-  function disableScroll() {
-    window.addEventListener('DOMMouseScroll', preventDefault, false);
-    window.onscroll = preventDefault;
-    window.onwheel = preventDefault;
-    window.onmousewheel = document.onmousewheel = preventDefault;
-    window.ontouchmove  = preventDefault;
-    document.onkeydown  = preventDefaultForScrollKeys;
+  function scrollDown() {
+    if (position == sections.length) {
+      animating = false;
+      return;
+    }
+
+    position = position + 1;
+    scroll(position * window.innerHeight);
   }
 
-  disableScroll();
+  function scroll(point) {
+    if (point > window.innerHeight * sections.length - window.innerHeight || point < 0) {
+      animating = false;
+      return;
+    }
 
-  window.addEventListener('swipeUp', function() {
-    console.log('Swipe up');
-  });
+    myself.style.transform = 'translateY(' + -point + 'px)';
 
-  window.addEventListener('swipeDown', function() {
-    console.log('Swipe down');
-  });
+    myself.addEventListener('transitionend', function() {
+      setTimeout(function() {
+        animating = false;
+      }, safety);
+    });
+  }
 });
+
+// Inspiration from:
+// github.com/peachananr/purejs-onepage-scroll/blob/master/onepagescroll.js#L173
+
+function swipe(element) {
+  var initial = 0;
+
+  document.addEventListener('touchstart', function(event) {
+    console.log('Sup');
+    var touches = event.touches;
+    if (touches && touches.length) {
+      initial = touches[0].pageY;
+      document.addEventListener('touchmove', touchMove);
+    }
+  }, false);
+
+  function touchMove(event) {
+    var touches = event.touches;
+
+    if (touches && touches.length) {
+      event.preventDefault();
+      var delta = initial - touches[0].pageY;
+
+      if (delta >= 50) {
+        var event = new Event('swipeUp');
+        document.dispatchEvent(event);
+      } else if (delta <= -50) {
+        var event = new Event('swipeDown');
+        document.dispatchEvent(event);
+      }
+
+      if (Math.abs(delta) >= 50) {
+        document.removeEventListener('touchmove', touchMove);
+      }
+    }
+  }
+}
+
+// MARK: - Prevent scrolling
+
+var keys = {
+  32 : 1,
+  37 : 1,
+  38 : 1,
+  39 : 1,
+  40 : 1
+};
+
+function preventDefault(event) {
+  event = event || window.event;
+
+  if (event.preventDefault) {
+    event.preventDefault();
+  }
+
+  event.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(event) {
+  if (keys[event.keyCode]) {
+    preventDefault(event);
+    return false;
+  }
+}
+
+function disableScroll() {
+  window.onscroll = preventDefault;
+  window.onmousewheel = preventDefault;
+  document.onkeydown = preventDefaultForScrollKeys;
+}
+
+disableScroll();
+
+// MARK: - Helper methods
+
+function prepareWindowFrames() {
+  window.requestAnimationFrame = window.requestAnimationFrame
+  || window.webkitRequestAnimationFrame
+  || window.mozRequestAnimationFrame
+  || window.oRequestAnimationFrame
+  || window.msRequestAnimationFrame
+  || function(callback) { window.setTimeout(callback, 1000 / 60) };
+}
