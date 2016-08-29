c3b309609c140d885650c7867a1ed5c7b9d3ebc2
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-29 19:34:42 +0200

    Adds and tries the first parallax in lights

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -10,11 +10,41 @@ function Parallax() {
   this.prepare = function() {
     if (isDetail()) {
       this.method = this.detail;
+
+      var containers = document.classes('image-wrapper');
+
+      for (var i = 0; i < 2; i++) {
+        var image = document.createElement('img');
+
+        var source = './images/details/lights/parallax/first.png';
+        var width = '540px';
+        var left = '135px';
+        var right = 'auto';
+        var top = '0px';
+        var position = 0;
+
+        if (i == 1) {
+          source = './images/details/lights/parallax/second.png';
+          left = '120px';
+          position = 2;
+        }
+
+        image.classList.add('parallax');
+        image.src = source;
+        image.style.position = 'absolute';
+        image.style.width = width;
+        image.style.top = top;
+        image.style.right = right;
+        image.style.left = left;
+
+        containers[position].appendChild(image);
+      }
     }
   }
 
   this.detail = function() {
     var headerText = document.class('hero-text-wrapper');
+    var images = document.classes('parallax');
 
     var height = window.innerHeight;
     var offset = window.pageYOffset;
@@ -29,7 +59,7 @@ function Parallax() {
       var positioning = positionOffset(image);
 
       if (positioning <= offset && positioning + image.clientHeight + height >= offset) {
-        var value = -(offset - positioning) * 50 / (height + image.clientHeight);
+        var value = (offset - positioning) * 20 / (height + image.clientHeight);
         image.style.transform = 'translate3d(0, ' + value + '%, 0)';
       }
     }
