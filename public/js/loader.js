9f417d8957c494020f7781be04d620c7d17bf8ea
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-08-30 13:31:33 +0200

    Done with the loader

--- public/js/loader.js
+++ public/js/loader.js
@@ -39,72 +39,100 @@ function Loader() {
 
   this.loaded = [];
   this.amount = 0;
-  this.sides = 'right 0.3s ease, left 0.8s ease';
+  this.actualPercentage = 0;
+  this.sides = 'right 0.15s linear, left 0.8s ease';
   this.opacity = 'opacity 0.8s ease';
   this.transformation = 'transform 0.8s ease';
 
   this.preload = function(callback) {
+    var startingTime = new Date().getTime();
     var page = document.id('loader');
 
     var bar = document.class('bar');
     var link = document.class('link-wrapper');
     var percentage = document.class('percentage');
+    var self = this;
 
     page.style.transition = this.opacity;
-    bar.style.transition = this.transformation;
-    link.style.transition = this.sides;
+    bar.style.transition = this.sides;
+    link.style.transition = this.transformation;
     percentage.style.transition = this.transformation;
 
     if (sessionStorage['loaded'] != undefined) {
       out(false);
-
       return;
     }
 
-    var self = this;
+    link.style.transform = 'translate(-50%, ' + 0 + '%)';
+    percentage.style.transform = 'translateX(0px)';
+    percentage.innerHTML = '0%';
 
     function done() {
       self.amount = self.amount + 1;
 
-      var percentage = window.innerWidth * self.amount / self.images.length;
-      bar.style.right = (100 - percentage) + '%';
+      var value = 100 * self.amount / self.images.length;
+      bar.style.right = (100 - value) + '%';
 
-      if (self.amount == self.images.length) {
+      var i = Math.floor(self.actualPercentage);
+      self.actualPercentage = Math.floor(value);
+      numbers();
+
+      function numbers() {
+        setTimeout(function() {
+          percentage.innerHTML = i + '%';
+
+          i = i + 1;
+          if (i <= value) { numbers(); }
+        });
+      }
+
+      if (self.amount === self.images.length) {
         sessionStorage['loaded'] = true;
-        out();
+        setTimeout(function() { out(); }, 200);
       }
     }
 
     function out(animate) {
       animate = animate === undefined ? true : animate;
 
+      var currentTime = new Date().getTime()
+
       if (animate) {
         page.style.opacity = 0;
         bar.style.left = '100%';
-        link.style.transform = 'translate(-50%, ' + 20 + '%)';
-        percentage.style.transform = 'translateX(' + 100 + 'px)';
+        link.style.transform = 'translate(-50%, ' + 500 + '%)';
+        percentage.style.transform = 'translateX(200px)';
       }
 
       setTimeout(function() {
         page.style.display = 'none';
 
-        setTimeout(function() { callback(); }, 100);
-      }, animate ? 1400 : 0);
+        setTimeout(function() { callback(); }, 200);
+      }, animate ? 400 : 0);
     }
 
-    for (var i = 0; i < this.images.length; i++) {
-      var image = new Image();
-      image.src = self.images[i];
+    var i = 0;
+    setTimeout(function() { loop(); }, 1400);
 
-      image.addEventListener('load', function() {
-        done();
-      });
+    function loop() {
+      setTimeout(function() {
+        var image = new Image();
+        image.src = self.images[i];
 
-      image.addEventListener('error', function() {
-        done();
-      });
+        image.addEventListener('load', function() {
+          done();
+        });
 
-      self.loaded[i] = image;
+        image.addEventListener('error', function() {
+          done();
+        });
+
+        self.loaded[i] = image;
+
+        i = i + 1;
+
+        if (i < self.images.length) { loop(); }
+      }, 25);
     }
   }
 }
