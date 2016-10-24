b3ce1a798be03e7e7dca85324b666988a8d780ae
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-10-24 21:30:26 +0200

    Fixes the project structure

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
