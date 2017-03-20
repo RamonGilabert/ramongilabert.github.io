1ad0b5227c2268b09908a715d1b4242a69c616fd
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2017-03-20 11:36:42 +0100

    Add an else

--- public/js/main.js
+++ public/js/main.js
@@ -342,6 +342,9 @@ function Resizer() {
         const gluten = document.class('gluten');
         const revolution = document.class('revolution');
 
+        console.log(gluten.offsetHeight);
+        console.log(revolution.offsetHeight);
+
         if (gluten === undefined || revolution === undefined) {
           setTimeout(function() {
             calculate();
